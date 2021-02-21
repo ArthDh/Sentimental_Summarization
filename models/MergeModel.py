8a58996848bc03075c33e4b259feafb726ea3a63
ArthDh <arth.dharaskar97@gmail.com>
2021-02-20 23:04:00 -0800

    Updated Scatter indexing

--- models/MergeModel.py
+++ models/MergeModel.py
@@ -11,7 +11,15 @@ class MergeModel(torch.nn.Module):
     
     def forward(self, article_id, article_mask, summary_id, summary_mask):
         self.summary_out = self.summary_model(article_id,  article_mask, summary_id, summary_mask)
+
+        idx =  torch.argmax(self.summary_out.logits, dim=2, keepdims=  True)
+        mask = torch.zeros_like(self.summary_out.logits).scatter_(2, idx, 1.)
+        
+        pred_summary_id = (self.summary_out.logits * mask).sum(axis=2).long()
+        pred_summary_mask = torch.ones_like(summary_mask).long()
+        pred_summary_id[:, -1] = 2
+        
         self.article_sentiment = self.sentiment_model(article_id,  article_mask, labels=None)
-        self.summary_sentiment = self.sentiment_model(summary_id,  summary_mask, labels=None)
+        self.summary_sentiment = self.sentiment_model(pred_summary_id,  pred_summary_mask, labels=None)
         
         return [self.summary_out, self.article_sentiment, self.summary_sentiment]
