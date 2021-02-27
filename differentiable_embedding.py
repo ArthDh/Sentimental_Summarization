2ace642847436c0c575704808b4096eb2be2726f
Sai Ashish <ee16btech11043@iith.ac.in>
2021-02-27 20:39:40 +0530

    Update differentiable_embedding.py

--- differentiable_embedding.py
+++ differentiable_embedding.py
@@ -1,13 +1,13 @@
 import torch
 import torch.autograd
 from torch.autograd import Variable
-
+import torch.nn.functional as F
 
 class Embedding_(torch.nn.Module):
     def __init__(self, vocab_size, embedding_dim):
         super(Embedding_, self).__init__()
         
-        self.embedding = torch.nn.Embedding.from_pretrained(self.weights)
+        self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
 
     def forward(self, mask):
         if mask.ndim == 2:
@@ -18,29 +18,41 @@ class Embedding_(torch.nn.Module):
         # here the mask is the one-hot encoding
         return torch.matmul(mask, self.embedding.weight)
 
-vocab_size = 3
-sent_len = 2
-embedding_dim = 5
-batch_size = 10
+class ClassifierModel(torch.nn.Module):
+    def __init__(self, 
+                 vocab_size,
+                 embedding_dim,
+                 out_dim,
+                 n_layers,
+                 hidden_size,
+                 dropout=0.5,
+                 batch_first=True,
+                 seq_length = 150
+                ):
+        super(ClassifierModel, self).__init__()
+        self.vocab_size = vocab_size
+        self.embedding = Embedding_(vocab_size, embedding_dim).requires_grad_()
+        self.lstm = torch.nn.LSTM(
+            embedding_dim,
+            hidden_size,
+            n_layers,
+            dropout=dropout,
+            batch_first=batch_first,
+        )
+        
+        fc_hidden = 32
+        self.fc1 = torch.nn.Linear(hidden_size*seq_length, fc_hidden)
+        self.fc2 = torch.nn.Linear(fc_hidden, out_dim)
+        
 
-# get logits
-x = Variable(torch.randint(0,10,(batch_size, sent_len, vocab_size)).float(), requires_grad = True)
-y = Variable(torch.randint(0,10,(batch_size, sent_len, vocab_size)).float(), requires_grad = True)
-z = y - 10
-t = x + 6
-logits = t/z
+    def forward(self, x):
+        embedding_out = self.embedding(x)
+        out, _ = self.lstm(embedding_out)
+        fc1 = F.relu(self.fc1(out.reshape(x.shape[0], -1)))
+        return self.fc2(fc1)
 
-# Masks
-idx =  torch.argmax(logits, dim=-1, keepdims=  True)
-mask = torch.zeros_like(logits).scatter_(-1, idx, 1.).float().detach() + logits - logits.detach()
+    def loss(self, x, target):
 
-# Find embedding
-emb_layer = Embedding(vocab_size, embedding_dim)
+        logits = self(x)
 
-embedding = emb_layer(mask)
-
-# Define some loss function to test the autograd
-loss = torch.sum(embedding)
-
-# test autograd
-print(torch.autograd.grad(loss, mask, retain_graph = True))
+        return loss_fn(logits, target)
