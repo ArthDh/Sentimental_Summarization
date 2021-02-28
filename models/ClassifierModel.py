b54a345dbb92f2240c2b9628cbb4594fe05a6460
ArthDh <arth.dharaskar97@gmail.com>
2021-02-27 18:26:43 -0800

    Updated MergeModel + overfit Classifier tests

---
+++ models/ClassifierModel.py
@@ -0,0 +1,59 @@
+import torch
+import torch.autograd
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class Embedding_(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(Embedding_, self).__init__()
+        
+        self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
+
+    def forward(self, mask):
+        if mask.ndim == 2:
+            assert mask.dtype == torch.long
+            return self.embedding(mask)
+        
+        assert mask.dtype == torch.float
+        # here the mask is the one-hot encoding
+        return torch.matmul(mask, self.embedding.weight)
+
+class ClassifierModel(torch.nn.Module):
+    def __init__(self, 
+                 vocab_size,
+                 embedding_dim,
+                 out_dim,
+                 n_layers,
+                 hidden_size,
+                 dropout=0.5,
+                 batch_first=True,
+                ):
+        super(ClassifierModel, self).__init__()
+        self.vocab_size = vocab_size
+        self.embedding = Embedding_(vocab_size, embedding_dim).requires_grad_()
+        self.lstm = torch.nn.LSTM(
+            embedding_dim,
+            hidden_size,
+            n_layers,
+            dropout=dropout,
+            batch_first=batch_first,
+        )
+        
+        fc_hidden = hidden_size//2
+        self.fc1 = torch.nn.Linear(hidden_size, fc_hidden)
+        self.fc2 = torch.nn.Linear(fc_hidden, out_dim)
+        
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        embedding_out = self.embedding(x)
+        out, _ = self.lstm(embedding_out)
+        out_summed = out.sum(axis=1)
+        fc1 = F.relu(self.fc1(out_summed.reshape(x.shape[0], -1)))
+        return self.fc2(fc1)
+
+    def loss(self, x, target):
+
+        logits = self(x)
+
+        return self.loss_fn(logits, target)
--- models/MergeModel.py
+++ models/MergeModel.py
@@ -5,24 +5,16 @@ class MergeModel(torch.nn.Module):
             Todo
     '''
     def __init__(self, summary_model, sentiment_model):
-        super().__init__()
+        super(MergeModel, self).__init__()
         self.summary_model = summary_model
         self.sentiment_model = sentiment_model
     
     def forward(self, article_id, article_mask, summary_id, summary_mask):
         self.summary_out = self.summary_model(article_id,  article_mask, summary_id, summary_mask)
+       
+        prob_logits,_ = torch.max(self.summary_out.logits,-1)
 
-        idx =  torch.argmax(self.summary_out.logits, dim=2, keepdims=  True)
-        mask = torch.zeros_like(self.summary_out.logits).scatter_(2, idx, 1.)
-        
-        
-        pred_summary_id = torch.argmax(self.summary_out.logits, 2).float() + self.summary_out.logits - self.summary_out.logits.detach()
-        print(pred_summary_id.shape)
-#         pred_summary_id = (self.summary_out.logits * mask).sum(axis=2).long()
-        pred_summary_mask = torch.ones_like(summary_mask).long()
-        pred_summary_id[:, -1] = 2
-        
-        self.article_sentiment = self.sentiment_model(article_id,  article_mask, labels=None)
-        self.summary_sentiment = self.sentiment_model(pred_summary_id,  pred_summary_mask, labels=None)
+        self.article_sentiment = self.sentiment_model(self.summary_out.logits)
+        self.summary_sentiment = self.sentiment_model(self.summary_out.logits)
         
         return [self.summary_out, self.article_sentiment, self.summary_sentiment]
