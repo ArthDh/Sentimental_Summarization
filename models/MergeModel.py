eae34ddccba0fc3c714cc03fd81d84083d89a76f
Sai Ashish <ee16btech11043@iith.ac.in>
2021-03-01 20:46:22 +0530

    Merged model - Gradients flow only w.r.t max value

--- models/MergeModel.py
+++ models/MergeModel.py
@@ -12,9 +12,10 @@ class MergeModel(torch.nn.Module):
     def forward(self, article_id, article_mask, summary_id, summary_mask):
         self.summary_out = self.summary_model(input_ids=article_id, attention_mask =article_mask, labels=summary_id, decoder_attention_mask=summary_mask, return_dict=True)
        
-        idx = torch.argmax(self.summary_out.logits, dim=-1, keepdims= True)
+        soft_idx, idx = torch.max(self.summary_out.logits, dim=-1, keepdims= True)
         
-        mask = torch.zeros_like(self.summary_out.logits).scatter_(-1, idx, 1.).float().detach() + self.summary_out.logits - self.summary_out.logits.detach()
+        
+        mask = torch.zeros_like(self.summary_out.logits).scatter_(-1, idx, 1.).float().detach() + soft_idx - soft_idx.detach()
 
         self.article_sentiment = self.sentiment_model(article_id)
         self.summary_sentiment = self.sentiment_model(mask)
